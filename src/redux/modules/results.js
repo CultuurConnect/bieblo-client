931d65ca9cd7d7883da92b537966e9420cfe3c8f
Jonas Verhaert <jonas.verhaert@kbc.be>
2017-03-06 12:32:20 +0100

    Added loaded = false again

--- src/redux/modules/results.js
+++ src/redux/modules/results.js
@@ -24,6 +24,7 @@ const reducer = (state = initialState, action = {}) => {
     case LOAD:
       return {
         ...state,
+        loaded: false,
         loading: true,
       }
     case LOAD_SUCCESS:
