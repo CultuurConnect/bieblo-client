d44434f4e7431942650f3dc883707265794c7144
Levi Van Zele <levi@punfyre.be>
2018-05-27 13:50:31 +0200

    Different initializing test

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -58,14 +58,14 @@ export default class Html extends Component {
         <script
           dangerouslySetInnerHTML={{
             __html:
-            '(function(h,o,t,j,a,r){\n' +
-            '    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};\n' +
-            '    h._hjSettings={hjid:895097,hjsv:6};\n' +
-            '    a=o.getElementsByTagName(\'head\')[0];\n' +
-            '    r=o.createElement(\'script\');r.async=1;\n' +
-            '    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;\n' +
-            '    a.appendChild(r);\n' +
-            '})(window,document,\'https://static.hotjar.com/c/hotjar-\',\'.js?sv=\');'
+            '(function(h,o,t,j,a,r){' +
+            '    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};' +
+            '    h._hjSettings={hjid:895097,hjsv:6};' +
+            '    a=o.getElementsByTagName("head")[0];' +
+            '    r=o.createElement("script");r.async=1;' +
+            '    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;' +
+            '    a.appendChild(r);' +
+            '})(window,document,"https://static.hotjar.com/c/hotjar-",".js?sv=");'
           }}
         />
       </head>
